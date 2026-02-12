f7a59c20e3686db1a1ed5248504f48314b058655
Alex Haslehurst <alex.haslehurst@gmail.com>
2026-02-12 13:29:25 +0000

    added (broken) html to image

--- ui/src/components/visualization/Controls.vue
+++ ui/src/components/visualization/Controls.vue
@@ -1,8 +1,20 @@
 <script setup lang="ts">
 import { useVueFlow } from '@vue-flow/core'
 import { OhVueIcon } from 'oh-vue-icons'
+import GvButton from '@/components/govuk/Button.vue'
+import { useScreenshot } from '@/components/visualization/useScreenshot'
 
-const { zoomIn, zoomOut, fitView } = useVueFlow()
+const { zoomIn, zoomOut, fitView, vueFlowRef } = useVueFlow()
+const { capture } = useScreenshot()
+
+function doScreenshot() {
+  if (!vueFlowRef.value) {
+    console.warn('VueFlow element not found')
+    return
+  }
+
+  capture(vueFlowRef.value)
+}
 </script>
 
 <template>
@@ -17,6 +29,7 @@ const { zoomIn, zoomOut, fitView } = useVueFlow()
       <button class="cursor-pointer" @click="fitView()">
         <OhVueIcon name="md-zoomoutmap" scale="2" />
       </button>
+      <GvButton @click="doScreenshot" class="mb-0!">Export as Image</GvButton>
     </div>
   </div>
 </template>
---
+++ ui/src/components/visualization/useScreenshot.ts
@@ -0,0 +1,44 @@
+import { toPng } from 'html-to-image'
+import { ref } from 'vue'
+
+export function useScreenshot() {
+  const error = ref<string | null>()
+
+  async function capture(el: HTMLElement) {
+    const fileName = `vue-flow-screenshot-${Date.now()}`
+
+    let data: string | null = null
+    error.value = null
+    try {
+      data = await toPng(el, {
+        quality: 1,
+        skipFonts: true,
+        backgroundColor: 'white',
+        style: {
+          border: 'none',
+        },
+        skipAutoScale: true,
+        filter(node) {
+          return !node.classList || !node.classList.contains('vue-flow__panel')
+        },
+      })
+    } catch (e) {
+      error.value = e instanceof Error ? e.message : String(e)
+      console.error(e)
+      return null
+    }
+
+    if (!data) return null
+
+    // immediately download the image if shouldDownload is true
+    const link = document.createElement('a')
+    link.download = `${fileName}.png`
+    link.href = data
+    link.click()
+  }
+
+  return {
+    capture,
+    error,
+  }
+}
