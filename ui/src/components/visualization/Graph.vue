537a37feaae5d69f0905a00cd4a0d88592009be2
Alex Haslehurst <alex.haslehurst@gmail.com>
2026-03-25 12:39:46 +0000

    remove export xlsx button from graph

--- ui/src/components/visualization/Graph.vue
+++ ui/src/components/visualization/Graph.vue
@@ -198,11 +198,6 @@ const DEFAULT_EDGE_PROPS: Partial<BaseEdgeProps> = {
     </div>
 
     <GvButton @click="layoutGraph" variant="secondary" class="mb-0!">Reset layout</GvButton>
-    <GvButton
-      @click="exportGroupStructureToXlsx(props.structure).then((buf) => downloadXlsx(buf, 'group-structure.xlsx'))"
-      variant="secondary"
-      class="mb-0!"
-    >Export xlsx</GvButton>
   </div>
 
   <section class="h-200" data-testid="graph-section">
